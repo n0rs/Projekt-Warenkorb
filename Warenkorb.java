bd964d503fe2102035401b49e86ce8d6858597f5
n0rs <n0rs9485@googlemail.com>
2024-10-03 17:31:45 +0200

    Test

--- Warenkorb.java
+++ Warenkorb.java
@@ -1,5 +1,6 @@
 class Warenkorb {
     public static void main(String[] args) {
         System.out.println("Wilkommen in ihrem digitalen Warenkorb. Viel Spaß und gutes shopping.");
+        // Hi
     }
 }
