fd2b2fa1f8c7f7d691dbbd9890d1366027e843bd
n0rs <n0rs9485@googlemail.com>
2024-10-03 17:27:26 +0200

    First Commit, Test

---
+++ README.md
@@ -0,0 +1,8 @@
+# Warenkorb Projekt Anwendungsentwicklung I
+todo
+## Beschreibung des Projekts
+todo
+### Klassen
+todo
+#### Klasse1 "Kunde"
+todo
---
+++ Warenkorb.java
@@ -0,0 +1,5 @@
+class Warenkorb {
+    public static void main(String[] args) {
+        System.out.println("Wilkommen in ihrem digitalen Warenkorb. Viel Spaß und gutes shopping.");
+    }
+}
