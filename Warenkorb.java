51c230fed0d8a36315269849247ed2d64f6bd895
n0rs <n0rs9485@googlemail.com>
2024-10-04 09:41:37 +0200

    Made class public

--- Warenkorb.java
+++ Warenkorb.java
@@ -1,4 +1,4 @@
-class Warenkorb {
+public class Warenkorb {
     public static void main(String[] args) {
         System.out.println("Wilkommen in ihrem digitalen Warenkorb. Viel Spaß und gutes shopping.");
         // Hi
