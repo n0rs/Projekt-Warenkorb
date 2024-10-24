0f6d446621fbf249cb03ab52ba0fa0cacd046cf6
n0rs <n0rs9485@googlemail.com>
2024-10-24 19:14:12 +0200

    Hi

--- Warenkorb/Warenkorb.java
+++ Warenkorb/Warenkorb.java
@@ -2,7 +2,7 @@ package Warenkorb;
 
 public class Warenkorb {
     public static void main(String[] args) {
-        System.out.println("Wilkommen in ihrem digitalen Warenkorb. Viel Spaß und gutes shopping.");
+        System.out.println("Wilkommen in ihrem Herr der Ringe Fanshop. Viel Spaß und gutes shopping.\n");
         Artikel.initiateWaren();
         Artikel.printWaren();
     }
