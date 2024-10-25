ca16d5871641404661a582fb48c5cf255da97d46
n0rs <n0rs9485@googlemail.com>
2024-10-25 09:12:30 +0200

    Added Attribute maxArtikel

--- Warenkorb/Warenkorb.java
+++ Warenkorb/Warenkorb.java
@@ -1,6 +1,7 @@
 package Warenkorb;
 
 public class Warenkorb {
+    int maxArtikel;
     public static void main(String[] args) {
         System.out.println("Wilkommen in ihrem Herr der Ringe Fanshop. Viel Spaß und gutes shopping.\n");
         Artikel.initiateWaren();
