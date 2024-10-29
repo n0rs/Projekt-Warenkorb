0d3e141df2d2a97e0b032cdc365b7d0cb528ec53
n0rs <n0rs9485@googlemail.com>
2024-10-29 19:24:38 +0100

    Hi

--- warenkorb/Main.java
+++ warenkorb/Main.java
@@ -3,14 +3,16 @@ package warenkorb;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Wilkommen in ihrem Herr der Ringe Fanshop. Viel Spaß und gutes shopping.\n");
-        Kunde k1 = new Kunde("abc", "abc", "abc", "abc");
-        Kunde k2 = new Kunde("cde", "abc", "abc", "abc");
+        Kunde k1 = new Kunde("Max", "Mustermann", "Musterstraße 3", "Musterstraße3");
+        Kunde k2 = new Kunde("Maxine", "Musterfrau", "Musterweg 12", "Musterweg 12");
         Artikel.initiateWaren();
         Artikel.printWaren();
         System.out.println(k1.getKundenNummer());
         System.out.println(k2.getKundenNummer());
         k1.addToWarenkorb(Artikel.waren.get(0));
         k2.addToWarenkorb(Artikel.waren.get(9));
+        System.out.println(k1.getLieferAdresse());
+        System.out.println(k2.getLieferAdresse());
         System.out.println(k1.getWarenkorb());
         System.out.println(k2.getWarenkorb());
     }
