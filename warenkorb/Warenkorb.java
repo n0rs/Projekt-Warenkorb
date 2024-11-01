aeeaf9935ca0560fbb82300be87077086dd5b6e4
n0rs <n0rs9485@googlemail.com>
2024-11-01 20:26:52 +0100

    hi

--- warenkorb/Warenkorb.java
+++ warenkorb/Warenkorb.java
@@ -4,90 +4,86 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Warenkorb {
-	// initiiert Warenkorb der Artikel speichern kann
-	private ArrayList<Artikel> warenkorb;
+    // initiiert Warenkorb der Artikel speichern kann
 
-	// Der Warenkorb Konstruktor erstellt nur eine ArrayList in der Artikel
-	// gespeichert werden können
-	public Warenkorb() {
-		this.warenkorb = new ArrayList<>();
-	}
+    private ArrayList<Artikel> warenkorb;
 
-	public void addWaren(Artikel artikel) {
-		warenkorb.add(artikel);
-	}
+    // Der Warenkorb Konstruktor erstellt nur eine ArrayList in der Artikel
+    // gespeichert werden können
+    public Warenkorb() {
+        this.warenkorb = new ArrayList<>();
+    }
 
-	public void changeWaren(Artikel artikel) {
-		// Eventuell reicht es die Anzahl zu ändern, bei 0 wird item gelöscht
-		// TODO
-	}
+    public void addWaren(Artikel artikel) {
+        warenkorb.add(artikel);
+    }
 
-	// (Liv) Rechnet den Preis der Artikel im Warenkorb mit einer for-schleife
-	// zusammen
-	// Ich musste hier Math.round benutzen & einmal *100 & /100 rechnen, damit keine
-	// Rundungsfehler rauskommen
-	public double getTotal() {
-		double total = 0;
-		for (int i = 0; i < warenkorb.size(); i++) {
-			total += (warenkorb.get(i).getArtikelPreis());
-		}
+    // (Liv) Rechnet den Preis der Artikel im Warenkorb mit einer for-schleife
+    // zusammen
+    // Ich musste hier Math.round benutzen & einmal *100 & /100 rechnen, damit keine
+    // Rundungsfehler rauskommen
+    public double getTotal() {
+        double total = 0;
+        for (int i = 0; i < warenkorb.size(); i++) {
+            total += (warenkorb.get(i).getArtikelPreis());
+        }
 
-		return Math.round(100.0 * total) / 100.0;
-	}
+        return Math.round(100.0 * total) / 100.0;
+    }
 
-	public void changeAnzahlW(Artikel artikel, int Anzahl) {
+    public void changeAnzahlW(Artikel artikel, int Anzahl) {
 
-		if (Anzahl > Collections.frequency(warenkorb, artikel)) {
-			int diff = Anzahl - Collections.frequency(warenkorb, artikel);
-			while (diff > 0) {
-				warenkorb.add(artikel);
-				diff--;
-			}
-		} else if (Anzahl < Collections.frequency(warenkorb, artikel)) {
-			int diff = Collections.frequency(warenkorb, artikel) - Anzahl;
-			while (diff > 0) {
-				warenkorb.remove(artikel);
-				diff--;
-			}
-		} else if (Anzahl == Collections.frequency(warenkorb, artikel)) {
-			System.out.println(
-					"Hinweis: '" + artikel.getArtikelBezeichnung() + "' ist bereits " + Anzahl + "-mal vorhanden.");
-		} else if (Anzahl == 0) {
-			removeallArtikelx(artikel);
-		}
-	}
+        if (Anzahl > Collections.frequency(warenkorb, artikel)) {
+            int diff = Anzahl - Collections.frequency(warenkorb, artikel);
+            while (diff > 0) {
+                warenkorb.add(artikel);
+                diff--;
+            }
+        } else if (Anzahl < Collections.frequency(warenkorb, artikel)) {
+            int diff = Collections.frequency(warenkorb, artikel) - Anzahl;
+            while (diff > 0) {
+                warenkorb.remove(artikel);
+                diff--;
+            }
+        } else if (Anzahl == Collections.frequency(warenkorb, artikel)) {
+            System.out.println(
+                    "Hinweis: '" + artikel.getArtikelBezeichnung() + "' ist bereits " + Anzahl + "-mal vorhanden.");
+        } else if (Anzahl == 0) {
+            removeallArtikelx(artikel);
+        }
+    }
 
-	public void removeallArtikelx(Artikel artikel) {
-		int artikelx = Collections.frequency(warenkorb, artikel);
+    public void removeallArtikelx(Artikel artikel) {
+        int artikelx = Collections.frequency(warenkorb, artikel);
 
-		while (artikelx > 0) {
-			warenkorb.remove(artikel);
-			artikelx--;
-		}
-	}
+        while (artikelx > 0) {
+            warenkorb.remove(artikel);
+            artikelx--;
+        }
+    }
 
-	// (Liv) entfernt alle Elemente aus dem Warenkorb ArrayList testtest
-	public void clearWaren() {
-		warenkorb.clear();
-	}
+    // (Liv) entfernt alle Elemente aus dem Warenkorb ArrayList testtest
+    public void clearWaren() {
+        warenkorb.clear();
+    }
 
-	// (Liv) getTotal() zu toString() hinzugefügt
-	@Override
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		for (Artikel artikel : warenkorb) {
-			sb.append(artikel.toString()).append("\n");
-		}
-		return sb.toString() + "Total: " + getTotal() + "€";
+    // (Liv) getTotal() zu toString() hinzugefügt
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Artikel artikel : warenkorb) {
+            sb.append(artikel.toString()).append("\n");
+        }
+        return sb.toString() + "Total: " + getTotal() + "€";
 
-	}
+    }
 
-	// Getter/Setter
-	public ArrayList<Artikel> getWarenkorb() {
-		return warenkorb;
-	}
+    // Getter/Setter
+    public ArrayList<Artikel> getWarenkorb() {
+        return warenkorb;
+    }
 
-	public void setWarenkorb(ArrayList<Artikel> warenkorb) {
-		this.warenkorb = warenkorb;
-	}
+    public void setWarenkorb(ArrayList<Artikel> warenkorb) {
+        this.warenkorb = warenkorb;
+    }
 }
