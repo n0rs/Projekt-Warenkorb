94aebc7673d110c562cf4783c80f20ec22cd2ce5
n0rs <n0rs9485@googlemail.com>
2024-11-05 15:56:55 +0100

    removed whitespaces

--- warenkorb/Main.java
+++ warenkorb/Main.java
@@ -6,28 +6,21 @@ public class Main {
 		Kunde k1 = new Kunde("Max", "Mustermann", "Musterstraße 3", "Musterstraße3");
 		Kunde k2 = new Kunde("Maxine", "Musterfrau", "Musterweg 12", "Musterweg 12");
 		Artikel.initiateWaren();
-//		Artikel.printWaren();
+		// Artikel.printWaren();
 		// System.out.println(k1.getKundenNummer());
 		// System.out.println(k2.getKundenNummer());
 		k1.addToWarenkorb(Artikel.waren.get(0),1);
 		k2.addToWarenkorb(Artikel.waren.get(9),3);
 		// System.out.println(k1.getLieferAdresse());
 		// System.out.println(k2.getLieferAdresse());
-
-		
 		System.out.println(k2.getWarenkorb());
-		
-		
-
 		System.out.println();
-		k1.addToWarenkorb(Artikel.waren.get(7));
-		k1.addToWarenkorb(Artikel.waren.get(8));
-		k2.addToWarenkorb(Artikel.waren.get(7));
-		k2.addToWarenkorb(Artikel.waren.get(7));
-//		System.out.println(k1.getWarenkorb());
-
+		// k1.addToWarenkorb(Artikel.waren.get(7));
+		// k1.addToWarenkorb(Artikel.waren.get(8));
+		// k2.addToWarenkorb(Artikel.waren.get(7));
+		// k2.addToWarenkorb(Artikel.waren.get(7));
+		// System.out.println(k1.getWarenkorb());
 		Kunde k3 = Kunde.kundenKontoErstellen();
 		System.out.println(k3.getKundenNummer());
-
 	}
 }
