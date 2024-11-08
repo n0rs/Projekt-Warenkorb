5773db0081aa7e2136559e6733b4aa4a74669406
n0rs <107929785+n0rs@users.noreply.github.com>
2024-11-08 12:41:18 +0100

    Update Main.java

--- warenkorb/Main.java
+++ warenkorb/Main.java
@@ -11,8 +11,7 @@ public class Main {
 
 		// begin the program
 		while (running == true) {
-			System.out
-					.println("Wilkommen in Ihrem Herr der Ringe Fanshop. Wir wünschen viel Spaß und gutes Shopping!\n");
+			System.out.println("Wilkommen in Ihrem Herr der Ringe Fanshop. Wir wünschen viel Spaß und gutes Shopping!\n");
 			Kunde k1 = new Kunde("Max", "Mustermann", "Musterstraße 3", "Musterstraße3");
 			Kunde k2 = new Kunde("Maxine", "Musterfrau", "Musterweg 12", "Musterweg 12");
 			Kunde k3 = Kunde.kundenKontoErstellen();
@@ -31,11 +30,11 @@ public class Main {
 				// k2.addToWarenkorb(Artikel.waren.get(7),3);
 				// System.out.println();
 				// System.out.println(k3.getKundenNummer());
+				k3.endBestellung();
 				running = false;
 			} else if (i.equals("N")) {
 				running = false;
 			}
-			k3.endBestellung();
 		}
 	}
 }
