3adaac213a0e4a4da44905001c9073686c31978a
lgr545 <livhanna.grimsehlschmitz@rfh-campus.de>
2024-11-08 12:09:35 +0100

    NeuCommitPush

--- warenkorb/Warenkorb.java
+++ warenkorb/Warenkorb.java
@@ -60,30 +60,31 @@ public class Warenkorb {
 	// verändert die Anzahl eines Artikels im Warenkorb
 	public void changeAnzahlW(Artikel artikel, int Anzahl) {
 
+		// Überprüfung max. Anzahl
 		if (warenkorb.size() + Anzahl <= 100 && Anzahl >= 0) {
 
-			if (Anzahl > Collections.frequency(warenkorb, artikel)) {
+			if (Anzahl > Collections.frequency(warenkorb, artikel)) { // Anzahl erhöhen
 				int diff = Anzahl - Collections.frequency(warenkorb, artikel);
 				while (diff > 0) {
 					warenkorb.add(artikel);
 					diff--;
 				}
-			} else if (Anzahl < Collections.frequency(warenkorb, artikel)) {
+			} else if (Anzahl < Collections.frequency(warenkorb, artikel)) { // Anzahl verringern
 				int diff = Collections.frequency(warenkorb, artikel) - Anzahl;
 				while (diff > 0) {
 					warenkorb.remove(artikel);
 					diff--;
 				}
-			} else if (Anzahl == Collections.frequency(warenkorb, artikel)) {
+			} else if (Anzahl == Collections.frequency(warenkorb, artikel)) { // Anzahl entspricht vorhandener Anzahl
 				System.out.println(
 						"Hinweis: '" + artikel.getArtikelBezeichnung() + "' ist bereits " + Anzahl + "-mal vorhanden.");
-			} else if (Anzahl == 0) {
+			} else if (Anzahl == 0) { // Anzahl 0 = Artikel werden entfernt
 				removeallArtikelx(artikel);
 			}
 
-		} else if (Anzahl < 0) {
+		} else if (Anzahl < 0) { // Fall: negative/ungültige Eingabe
 			System.out.println("Ungültige Eingabe. Anzahl darf keinen negativen Wert haben.");
-		} else {
+		} else { // Fall: über 100 Artikel
 			System.out.println(
 					"Warenkorb fasst max. 100 Artikel. Ihr aktueller Warenkorb: " + warenkorb.size() + " Elemente.");
 		}
